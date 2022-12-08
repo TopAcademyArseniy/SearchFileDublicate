3efa662cceb2ce188845e875a885772c67745189
1PixelDemon1 <pxldem@gmail.com>
2022-12-09 01:52:18 +0500

    Implemented data storage.

---
+++ FileExplorer/FileData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExplorer
+{
+    internal class FileData
+    {
+        //Необходимо реализовать информационную
+        //систему «Поиск дубликатов файлов»
+        //с учетом одного или нескольких из следующих
+        //атрибутов: имя, дата создания/изменения, размер
+
+        public string Name { get; set; }
+        public DateTime CreationTime { get; set; }
+        public long Size { get; set; }
+
+
+        public FileData(string name, DateTime creationTime, long size)
+        {
+            Name = name;
+            CreationTime = creationTime;
+            Size = size;
+        }
+
+        public FileData(FileInfo info)
+        {
+            Name = info.Name;
+            CreationTime = info.CreationTime;
+            Size = info.Length;
+        }
+
+        public override string ToString()
+        {
+            return $"Filename:\t{Name}\nCreation time:\t{CreationTime}\nFile size:\t{Size} bytes\n";
+        }
+    }
+}
---
+++ FileExplorer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace FileExplorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryData dirData = new DirectoryData("D:\\Users\\winpx\\downloads");
+            foreach (var data in dirData.getFileDatas())
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
